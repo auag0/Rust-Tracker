b48f36ab045f0b05fff83868ac88b0650bed79ba
anago <outpe1119@gmail.com>
2023-10-18 20:49:23 +0900

    プロジェクト ファイルを追加します。

---
+++ Rust-Tracker/Data/SavedPlayerDataManager.cs
@@ -0,0 +1,27 @@
+﻿using Rust_Tracker.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+namespace Rust_Tracker.Data
+{
+    internal class SavedPlayerDataManager
+    {
+        private static readonly string DATA_FILE_NAME = "SavedPlayerList.json";
+        private static SavedPlayerDataManager? Instance;
+        public static SavedPlayerDataManager GetInstance()
+        {
+            return Instance ??= new SavedPlayerDataManager(); 
+        }
+
+        public void SavePlayerList(List<SavedPlayer> players)
+        {
+            FileStream fileStream = File.Open(DATA_FILE_NAME, FileMode.OpenOrCreate);
+            JsonSerializer.Serialize(fileStream, players);
+        }
+    }
+}
---
+++ Rust-Tracker/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using Rust_Tracker.Data;
+using Rust_Tracker.Models;
+using Rust_Tracker.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Rust_Tracker
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            List<SavedPlayer> players = new List<SavedPlayer>();
+            players.Add(new SavedPlayer(1149045620));
+            players.Add(new SavedPlayer(1104663416));
+            players.Add(new SavedPlayer(935652611));
+
+            SavedPlayerDataManager.GetInstance().SavePlayerList(players);
+        }
+
+        private void navBtnSavedPlayerList_Click(object sender, RoutedEventArgs e)
+        {
+            main_content.Navigate(new SavedPlayerList());
+        }
+    }
+}
